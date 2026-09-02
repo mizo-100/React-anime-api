c0d9c6efda8e844babbebbd0b2294db4d27f903e
kairi <mizobata.100@gmail.com>
2026-09-03 01:42:21 +0900

    main commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600 underline">
+        Tailwind CSS が有効になりました！
+      </h1>
+    </div>
+  );
+}
+
+export default App
